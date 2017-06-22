f6eb24c5c3dd1a140097d982af9b36fb2db56766
Kamal <kamaljot.saini97@gmail.com>
2017-06-21 21:08:50 -0700

    Kamal - Before merging DeleteBook function to his laptop

--- CombinedProject22B/CombinedProject22B/Main.cpp
+++ CombinedProject22B/CombinedProject22B/Main.cpp
@@ -908,9 +908,11 @@ bool deleteBookJune21(string file_name, string inputISBN_Title, int whichOne)
 	ifstream fileSource;
 	ofstream buffer;
 
+
 	//Create 2 objects of Inventory Class
 	Inventory* copyObject = new Inventory;
 	Inventory* runnerObject = new Inventory;
+
 
 	//Open fileSource for input
 	fileSource.open(file_name);
@@ -933,9 +935,9 @@ bool deleteBookJune21(string file_name, string inputISBN_Title, int whichOne)
 
 			//cout << "*runnerObject BELOW: " << endl << *runnerObject;
 			//While loop to read data goes BELOW
-			while (!fileSource.eof())
+			while (getline(fileSource, fileLine_input))
 			{
-				fileSource >> *runnerObject;
+				
 			}
 
 		}
